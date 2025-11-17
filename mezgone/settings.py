a87a84bb0ac368e8237ba71a476006dbeace7382
Achel <achelmichael80@gmail.com>
2025-11-17 15:05:16 +0000

    mezz

--- mezgone/settings.py
+++ mezgone/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Use absolute static URL so links resolve correctly on any path
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'templates' / 'assets',  # serve css/js from templates/assets during development
 ]
